7970ee531a415eddfd98dbf7617be995b64f19c8
Feras al sabaa <feras.alsabaa@uni-weimar.de>
2018-04-29 10:41:59 +0200

    two files vec.cpp and vec.hpp

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -3,8 +3,27 @@
 #include<cmath>
 #include "vec2.hpp"
 
+  Vec2 vector_zero;
+
+TEST_CASE("describe_vec", "[vec]")
+{
+  REQUIRE(vector_zero.x_ == 0.0f );
+  REQUIRE(vector_zero.y_ == 0.0f );
+} 
+ 
+~Vec2();
+
+  Vec2 vector_numbers(2.0f, 3.0f );
+
+TEST_CASE("describe_vec2", "[vec2]")
+{
+  REQUIRE(vector_numbers.x_ == 2.0f );
+  REQUIRE(vector_numbers.y_ == 3.0f );
+} 
+
 
 int main(int argc, char* argv[])
-{ Vec2 vector1();
+{ 
+  
   return Catch::Session().run(argc, argv);
 }
--- source/vec2.hpp
+++ source/vec2.hpp
@@ -1,24 +1,26 @@
+#include"iostream"
 # ifndef VEC2_HPP
 # define VEC2_HPP
-#include <catch.hpp>
+
 
 
 struct Vec2
 {
-float x_ ;
-float y_ ;
+ float x_ ;
+ float y_ ;
     Vec2(){
      float x_ = 0.0f;
      float y_ = 0.0f;
     }
-  TEST_CASE("describe_Vec2_constructor", "[Vec2]"){
-  REQUIRE(Vec2() == 0);
-  REQUIRE(gcd(9,6) == 3);
-  REQUIRE(gcd(3,7) == 1);
-}
-    Vec2(float x_,float y_){
-    x_ = x_;
-    y_ = y_;
+    ~Vec2()
+    {
+      std:: cout << "bla bla" ;
     }
+ 
+    Vec2(float x_,float y_){
+     x_ = x_;
+     y_  = y_;
+    } 
+    
 };
 # endif
