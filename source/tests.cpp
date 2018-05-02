454a1341059c582ab37df4ab4c32a9dd0ec21c68
Feras al sabaa <feras.alsabaa@uni-weimar.de>
2018-05-02 12:20:09 +0200

    mehrere Teste fuer die Methode operator *=

--- source/tests.cpp
+++ source/tests.cpp
@@ -53,6 +53,10 @@ TEST_CASE("describe_vec2_*= ", "[vec2]")
    v1.operator*= (3);
    REQUIRE(v1.x_ == 6.0f );
    REQUIRE(v1.y_ == 9.0f );
+   Vec2 v2(2.22f, 3.55f );
+   v2.operator*= (0);
+   REQUIRE(v2.x_ == 0.0f );
+   REQUIRE(v2.y_ == 0.0f );
 } 
 TEST_CASE("describe_vec2_/= ", "[vec2]")
 {
